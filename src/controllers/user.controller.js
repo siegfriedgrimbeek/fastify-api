c7ae1f4533c7350c524e5974c0a5ead424d66498
at-vinguyen <vi.nguyen@asiantech.vn>
2018-12-15 17:54:44 +0700

    adding user model

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const constructor = require('../core/base/controller')
+let { actions, boom, model } = constructor('User')
+
+/**
+ * Adding new action here
+ */
+
+module.exports = actions
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 // Import routes
 const bookRoutes = require('./book.routes')
 const carRoutes = require('./car.routes')
+const userRoutes = require('./user.routes')
 
 module.exports = [
 ...bookRoutes,
-...carRoutes
+...carRoutes,
+...userRoutes
 ]
