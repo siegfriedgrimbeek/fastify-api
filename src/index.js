c46d3deb4f4cb995f0ac6919ab6ef6db2d309a87
Daniil Viliavin <89505105005@mail.ru>
2020-11-21 14:54:18 +0300

    Change host

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ testResultsRoutes.forEach((route, index) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT, 'localhost')
+    await fastify.listen(process.env.PORT, '0.0.0.0')
     fastify.swagger()
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
